430e868aa4a91fa8b1a5235b489139ee9a43aa63
Drabinskyi Vasiliy <facial91@gmail.com>
2025-09-21 17:39:53 +0300

    Added Task 1

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,34 @@
-namespace DefaultNamespace;
+namespace WorkingWithArrays2;
 
 public class Program
 {
+    #region Task 1
+
+    int[] arrays = { 44, 5, 86, 23, 98, 12, 100, 120, 1, 34, 199, 187 };
+
+    int biggestNumber = 0;
+    int enultimateLargeNumber = 0;
+
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        if (biggestNumber < arrays[i])
+        {
+            enultimateLargeNumber = biggestNumber;
+            biggestNumber = arrays[i];
+        }
+        else if (arrays[i] > enultimateLargeNumber && arrays[i] < biggestNumber)
+        {
+            enultimateLargeNumber = arrays[i];
+        }
+    }
+
+    Console.WriteLine($"The largest number in the array is {biggestNumber}, and the second to last large number is {enultimateLargeNumber}.");
+
+    #endregion
     
+    #region Task 2
+    
+    
+    
+    #endregion
 }
